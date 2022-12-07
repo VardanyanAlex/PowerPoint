299a8f5bc48ea98c371176f676f1a5358cf4bb5e
VardanyanAlex <90654986+VardanyanAlex@users.noreply.github.com>
2022-12-07 09:24:44 +0400

    wrong file

--- PowerPoint/variablesInitParse.hpp
+++
@@ -1,9 +0,0 @@
-
-#ifndef VARIABLESINITPARSE_HPP
-#define VARIABLESINITPARSE_HPP
-
-#include "tokenizer.hpp"
-
-Tree&& ParseVarInit(VariablesData&, FunctionsData&, std::istream& Input, SymbolTable&);
-
-#endif // VARIABLESINITPARSE_HPP
